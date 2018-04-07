ec442caf9633da01c069a0fa513a622188e773ab
stanimir003 <37873660+stanimir003@users.noreply.github.com>
2018-04-07 10:22:09 +0300

    Add files via upload

---
+++ Simple Loops/Java03_Latin_Letters.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Simple_Loops;
+
+/**
+ *
+ * @author admin
+ */
+public class Java03_Latin_Letters {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (char i = 'a'; i < 'z'; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
