b3ba3ebe70b896bc4b4818e72d8467a6566507ff
stanimir003 <37873660+stanimir003@users.noreply.github.com>
2018-04-18 23:39:03 +0300

    Add files via upload

---
+++ Drawing Figures with Loops/Java05_Square_Frame.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Drawing_Figures_with_Loops;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class Java05_Square_Frame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.print("+");
+        for (int i = 0; i < n - 2 ; i++) {
+           System.out.print(" -");
+            }
+        System.out.println(" +");
+        for (int row = 0; row < n-2; row++) {
+            System.out.print("|");
+            for (int i = 0; i < n-2; i++) {
+                System.out.print(" -");
+                
+            }
+            System.out.println(" |");
+        }
+         
+        System.out.print("+");
+        for (int i = 0; i < n -2 ; i++) {
+           System.out.print(" -");
+            }
+        System.out.print(" +");
+    }
+     
+    }
+
+   
+    
+
